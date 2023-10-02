49d526e31aafa8ad64c52f62518f443898b8acd9
khuyenarent <khuyen.nguyen@arent3d.com>
2023-10-02 15:55:23 +0700

    add icon

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from tkinter import *
+import random
+import tkinter
 
-root = Tk()
-root.geometry("500x500")
+hello = ["Xin chào! gf g fg f gf gf gf g fg f gf g fg f gf g fg f g", "Hellogfgf gf g fg fg f gf g fg fg", "新潮 fd f df df d fd f df", "シンチャオ d f d df df df"]
+number = random.randrange(0,3)
+window = tkinter.Tk()
+window.geometry('700x200')
+window.title("Hello world")
+tkinter.Label(window, text=hello[number], font="Helvetica 16 bold").pack()
 
-# Create label widget
-myLabel = Label(root, text="Hello World!")
-# Pack it onto the screen.
-myLabel.pack()
-
-root.mainloop()
+window.mainloop()
